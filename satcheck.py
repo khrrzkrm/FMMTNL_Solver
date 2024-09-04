c2188ce87ec6a864101c61d9be4c35c9b0911f53
karam <kharraz@isp.uni-luebeck.de>
2024-09-04 13:29:41 +0200

    ready to use

---
+++ satcheck.py
@@ -0,0 +1,13 @@
+import unittest
+from reminder import *
+from NTL_ParsertoAst import *
+from z3_precise import *
+from NTL_Struct import BinaryOperation
+def main():
+    # Ask the user for input
+    input_string = input("Enter a formula from FMMTNL: ")
+    parsed = process_input(input_string)
+    synthetize(parsed)
+if __name__ == "__main__":
+    main()
+    
